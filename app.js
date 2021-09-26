06b2ede7b914db1a2b44403c47eeb1dfbfdaedea
garry332211 <gurvindersinghoo7@hotmail.com>
2021-09-26 17:14:34 +1300

    back to normal

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || seper-backend.herokuapp.com;
+const port = process.env.PORT || 8082;
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
